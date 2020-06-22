6610d0356d0641262dc0e0a3da2cd24385991059
safinsingh <safin.singh@gmail.com>
2020-06-21 20:09:04 -0700

    add scss

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { Container } from 'reactstrap'
 import Login from './components/Login'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
+import './App.scss'
 
 class App extends Component {
   render() {
